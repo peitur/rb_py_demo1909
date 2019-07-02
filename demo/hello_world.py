570571067ca8ec3da931466b2f93c9700d9e35dc
눈_눈 <peitur@users.noreply.github.com>
2019-07-02 20:55:32 +0200

    Update hello_world.py

--- demo/hello_world.py
+++ demo/hello_world.py
@@ -1,5 +1,12 @@
 #!/usr/bin/env python3
 # Run python3, where ever it is
 
+# Buildin __name__ contains name of module.
+# as in other languages, main is entrypoint, this is where everything starts in some way.
 if __name__ == "__main__":
+  
+  # Scope defined by indentation, tab or space. 
+  # The type must be consistent!
+  
+  # Lets just print something
   print("Hello world")
