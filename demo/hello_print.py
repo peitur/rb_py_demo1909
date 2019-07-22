759d7161987ed35ba9ffb511aad9dd8e8a434d39
peitur <peitur@gmail.com>
2019-07-22 09:50:46 +0200

    skel for new examples, more advanced

--- demo/hello_print.py
+++ demo/hello_print.py
@@ -1,6 +1,11 @@
 #!/usr/bin/env python3
 
+import sys
 from pprint import pprint
 
 if __name__ == "__main__":
-    pass
+
+    print("your message")
+    print("your message", file=sys.stdout )
+    print("your message", file=sys.stderr)
+    print("your message", end="" )
