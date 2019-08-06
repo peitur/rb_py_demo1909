c6c8e715c30b3dea9e5f0cb004e4cf465cdf4803
peitur <peitur@gmail.com>
2019-08-06 14:26:08 +0200

    extremly simple regexp example

---
+++ demo/hello_regexp.py
@@ -0,0 +1,15 @@
+#!/usr/bin/env python3
+
+import sys, re, os
+from pprint import pprint
+
+if __name__ == "__main__":
+
+    data1 = ["abcd21", "bcdeaa", "cdefa4", "defh44", "21wjekfwk"]
+
+    pprint( data1 )
+
+    r1 = re.compile( r'[a-z]+([0-9]+)' )
+    for item in data1:
+        if r1.match( item ):
+            print( item )
