efb9086b7ad635164c56ecc8744286c18dd1cca1
peitur <peitur@gmail.com>
2019-08-23 15:41:13 +0200

    example jinja

--- demo/hello_jinja.py
+++ demo/hello_jinja.py
@@ -1,8 +1,16 @@
 #!/usr/bin/env python3
 
-import jinja2
+import json, os
 from pprint import pprint
-
+from jinja2 import Environment, FileSystemLoader, select_autoescape
 
 if __name__ == "__main__":
-    pass
+
+    jsn = json.load( open( "samples/jinja_data.json" ) )
+
+    pprint( jsn )
+
+    env = Environment( loader=FileSystemLoader('samples/'))
+    template = env.get_template('jinja_template.tmpl')
+
+    print( template.render( jsn ) )
