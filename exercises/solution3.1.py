d29ed00453dd5d93c5d5dbb60840a7452e309cc1
peitur <peitur@gmail.com>
2019-09-11 19:04:26 +0200

    removed unused function

--- exercises/solution3.1.py
+++ exercises/solution3.1.py
@@ -29,24 +29,6 @@ def run( cmd, **opt ):
         return (prc.returncode, result)
 
 
-
-def run_yield( cmd, **opt ):
-        debug = False
-        if 'debug' in opt and opt['debug'] in (True, False):
-            debug = opt['debug']
-
-        if type( cmd ).__name__ == "str":
-            cmd = shlex.split( cmd )
-
-        if debug: print( "Running: '%s'" % ( " ".join( cmd ) ) )
-
-        prc = subprocess.Popen( cmd, stdout = subprocess.PIPE, stderr = subprocess.STDOUT, universal_newlines=True )
-        for line in prc.stdout.readlines():
-            yield line.rstrip()
-
-            if prc.poll():
-                break
-
 def command_valid( command ):
 
     if type( command ).__name__ == "str":
