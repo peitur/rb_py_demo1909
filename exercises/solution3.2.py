58bb6a2c592dce4f532035c863d426abafd6abaf
peitur <peitur@gmail.com>
2019-09-11 18:35:52 +0200

    simple not so stable ping monitor

--- exercises/solution3.2.py
+++ exercises/solution3.2.py
@@ -5,12 +5,21 @@ import subprocess, shlex
 
 from pprint import pprint
 
-def run( cmd, **opt ):
+def run_ping( opt={} ):
         result = list()
         debug = False
+
         if 'debug' in opt and opt['debug'] in (True, False):
             debug = opt['debug']
 
+        numStr = ""
+        if 'num' in opt and opt["num"] and type( opt['num'] ).__name__ == "int":
+            numStr = "-c %s" % ( opt["num"] )
+
+        if 'host' not in opt:
+            raise AttributeError("Missing host")
+
+        cmd = "ping %s %s" % ( numStr, opt['host'] )
         if type( cmd ).__name__ == "str":
             cmd = shlex.split( cmd )
 
@@ -18,33 +27,25 @@ def run( cmd, **opt ):
 
         prc = subprocess.Popen( cmd, stdout = subprocess.PIPE, stderr = subprocess.STDOUT, universal_newlines=True )
         for line in prc.stdout.readlines():
-            result.append( line )
             if prc.poll():
                 break
 
-        return (prc.returncode, result)
+        return prc.returncode
 
 
-
-def run_yield( cmd, **opt ):
-        debug = False
-        if 'debug' in opt and opt['debug'] in (True, False):
-            debug = opt['debug']
-
-        if type( cmd ).__name__ == "str":
-            cmd = shlex.split( cmd )
-
-        if debug: print( "Running: '%s'" % ( " ".join( cmd ) ) )
-
-        prc = subprocess.Popen( cmd, stdout = subprocess.PIPE, stderr = subprocess.STDOUT, universal_newlines=True )
-        for line in prc.stdout.readlines():
-            yield line.rstrip()
-
-            if prc.poll():
-                break
 
 
 from pprint import pprint
 
 if __name__ == "__main__":
-    pass
+
+    options = dict()
+    if len( sys.argv ) > 1:
+        options['host'] = sys.argv[1]
+
+    if len( sys.argv ) > 2:
+        options['num'] = int( sys.argv[2] )
+
+    rch = run_ping( options )
+    if rch != 0:
+        raise RuntimeError( "Ping failed to connect to %s" % ( options['host'] ) )
