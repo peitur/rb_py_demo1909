472744f0a91c973e5f8d81cb33daf6665b2cd756
peitur <peitur@gmail.com>
2019-10-01 10:28:28 +0200

    minor updates and started on sqlite demo

--- demo/hello_strings.py
+++ demo/hello_strings.py
@@ -4,7 +4,7 @@ if __name__ == "__main__":
 
     ## empty list of whatever, empty
     str0 = ""
-    print( dir( str0 ) )
+#    print( dir( str0 ) )
 
     print("#################################################")
     ## simple build stringh with variables
@@ -12,4 +12,4 @@ if __name__ == "__main__":
     print( "\n[s2] hello %30s again" % ( "world" ) )
     print( "\n[s3] hello %-30s again" % ( "world" ) )
     print( "\n[s4] hello again for the %d time" % ( 10 ) )
-    print( "\n[s5] hello %(name)s for %(time)s" % {"name":"world", "time":"last" } )
+    print( "\n[s5] hello %(name)s for %(time)s" % { "time":"last", "name":"world" } )
