c219725d4a5d5c44b34b05be066f8f32857c7a6f
peitur <peitur@gmail.com>
2019-11-05 12:55:20 +0100

    split logging examples

--- demo/hello_logger.py
+++ demo/hello_logger.py
@@ -1,12 +1,26 @@
 #!/usr/bin/env python3
 
-import logging
+import logging, logging.handlers
 from pprint import pprint
 
-if __name__ == "__main__":
-    logging.basicConfig( datefmt="%Y%m%d:%H%M%S %z",  level=logging.DEBUG, format="%(asctime)s - %(levelname)s - %(name)s - %(message)s")
+logger = None
 
-    logging.info("Testing")
-    logging.warning("Not as expected")
-    logging.debug("knowing your stuff?")
-    logging.critical("Oooops!")
+if __name__ == "__main__":
+
+    logger = logging.getLogger( __name__ )
+    formatter = "%(asctime)s - %(levelname)s - %(name)s - %(message)s"
+
+    outhandler = logging.StreamHandler()
+    syshandler = logging.handlers.SysLogHandler( address='/dev/log', facility=logging.handlers.LOG_USER )
+
+    outhandler.setFormatter( formatter )
+    syshandler.setFormatter( formatter )
+
+    outhandler.setLevel( logging.DEBUG )
+
+    logger.addHandler( outhandler )
+    logger.addHandler( syshandler )
+
+
+    for i in range(:2048):
+        logger.info( "Log message %s" % (i) )
